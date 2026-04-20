bbdf6196f1e9600e22dfc228ebf24c8f624b1e2d
Mahabub Hasan Mamun <102401671+mahabubmamun@users.noreply.github.com>
2026-04-21 02:06:09 +0600

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # mp : Dict[str, List[str]] = {}
+        mp = defaultdict(list)
+        for it in strs:
+            key = it
+            sorted_key = "".join(sorted(key))
+            # mp.setdefault(sorted_key, []).append(it)
+            mp[sorted_key].append(it)
+        # result : List[List[str]] = []
+        # for x in mp:
+        #     result.append(mp[x])
+
+        return list(mp.values())
